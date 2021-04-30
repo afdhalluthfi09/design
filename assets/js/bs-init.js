eba8d0069652171fa0985930dd6d867f8dcfeb81
yudi <jogjaplaza@gmail.com>
2021-04-30 15:17:52 +0700

    tambahan

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -3,4 +3,9 @@ $(document).ready(function(){
 	$('[data-bss-hover-animate]')
 		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
 		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
+
+	$('[data-bss-chart]').each(function(index, elem) {
+		this.chart = new Chart($(elem), $(elem).data('bss-chart'));
+	});
+
 });
